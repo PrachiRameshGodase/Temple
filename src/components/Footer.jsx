3da5a2325cf5153397a8a3257bc649234e4a5d46
aryan7122 <aryankushwahalifenote@gmail.com>
2025-02-27 18:21:53 +0530

    aryan V 0.0.06 commit branch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer-container">
-    <div className="footer-line"></div>
+      <div className="footer-line"></div>
 
-    <div className="footer-content">
-      <p>© Copyright 2025 — Sri Shirdi Sai Anandamaiyee Temple. All rights reserved.</p>
-      <ul>
-        <li>Privacy Policy</li>
-        <li>Terms of Service</li>
-        <li>Cookies Settings</li>
-      </ul>
-    </div>
-  </footer>
+      <div className="footer-content">
+        <p>© Copyright 2025 — Sri Shirdi Sai Anandamaiyee Temple. All rights reserved.</p>
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Terms of Service</li>
+          <li>Cookies Settings</li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
