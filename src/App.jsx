7e813867ebc0d481518949e08b7e8c00d855a5ed
aryan7122 <aryankushwahalifenote@gmail.com>
2025-02-27 16:51:51 +0530

    aryan V 0.0.04

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
                 <StatsSection />
                 <SocialServices />
                 <VoiceOfDevotees />
-                <TempleLocation/>
+                <TempleLocation />
               </>
             }
           />
