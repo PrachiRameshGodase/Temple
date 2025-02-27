9c977dc16ded67c0be464b4ef43de5c2074ffb5b
PrachiRameshGodase <prachigodase2@gmail.com>
2025-02-27 10:41:08 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => (
+  <div>
+    <div>
+      <div>
+        <img src='' alt='logo'></img>
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Social Services & Experiences</li>
+          <li>Upcoming Projects</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div>
+        <button></button>
+      </div>
+    </div>
+  </div>
+)
+
+export default Navbar
