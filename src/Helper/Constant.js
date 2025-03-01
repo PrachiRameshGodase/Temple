0aa405258525289248e5d6c21663bbfb50e2e057
aryan7122 <aryankushwahalifenote@gmail.com>
2025-03-01 11:27:11 +0530

    aryan V 0.0.07

--- src/Helper/Constant.js
+++ src/Helper/Constant.js
@@ -1,17 +1,48 @@
 import { otherIcons } from "./icons";
 
+import img1 from '../assets/templeSnapShot/IMG-20250301-WA0002_15_11zon.jpg'
+import img2 from '../assets/templeSnapShot/IMG-20250301-WA0003_14_11zon.jpg'
+import img3 from '../assets/templeSnapShot/IMG-20250301-WA0004_12_11zon.jpg'
+import img4 from '../assets/templeSnapShot/IMG-20250301-WA0005_13_11zon.jpg'
+import img5 from '../assets/templeSnapShot/IMG-20250301-WA0006_11_11zon.jpg'
+import img6 from '../assets/templeSnapShot/IMG-20250301-WA0007_10_11zon.jpg'
+import img7 from '../assets/templeSnapShot/IMG-20250301-WA0008_9_11zon.jpg'
+import img8 from '../assets/templeSnapShot/IMG-20250301-WA0009_6_11zon.jpg'
+import img9 from '../assets/templeSnapShot/IMG-20250301-WA0010_7_11zon.jpg'
+import img10 from '../assets/templeSnapShot/IMG-20250301-WA0011_8_11zon.jpg'
+import img11 from '../assets/templeSnapShot/IMG-20250301-WA0012_4_11zon.jpg'
+import img12 from '../assets/templeSnapShot/IMG-20250301-WA0013_5_11zon.jpg'
+import img13 from '../assets/templeSnapShot/IMG-20250301-WA0014_3_11zon.jpg'
+import img14 from '../assets/templeSnapShot/IMG-20250301-WA0015_2_11zon.jpg'
+import img15 from '../assets/templeSnapShot/IMG-20250301-WA0016_1_11zon.jpg'
+
 export const temple = "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/hc8ridnfhsp1up5ot19y"
 
 export const templeSnapShot = [
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/sxwabhip4clcn0oroqjk" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/kfogwhzmuazrosnsikmk" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/bfa4sf6gkcod1tdr6aef" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/wq6gdjsgtg1kytwdwbmq" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/egb3zr0ejz39j3ccaxaw" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/bxxgts9yo9cprwsd6m1a" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/jmoctlz4yakukyrwwohc" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/fuw6okoqtumsdrujfk9h" },
-    { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/pxfvhbgdpusy1w1ahuap" },
+    { imgUrl:img1},
+    { imgUrl:img2},
+    { imgUrl:img3},
+    { imgUrl:img4},
+    { imgUrl:img5},
+    { imgUrl:img6},
+    { imgUrl:img7},
+    { imgUrl:img8},
+    { imgUrl:img9},
+    { imgUrl:img10},
+    { imgUrl:img11},
+    { imgUrl:img12},
+    { imgUrl:img13},
+    { imgUrl:img14},
+    { imgUrl:img15},
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/sxwabhip4clcn0oroqjk" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/kfogwhzmuazrosnsikmk" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/bfa4sf6gkcod1tdr6aef" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/wq6gdjsgtg1kytwdwbmq" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/egb3zr0ejz39j3ccaxaw" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/bxxgts9yo9cprwsd6m1a" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/jmoctlz4yakukyrwwohc" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/fuw6okoqtumsdrujfk9h" },
+    // { imgUrl: "https://res.cloudinary.com/dkxfvxdca/image/upload/f_auto,q_auto/v1/Temple/About/pxfvhbgdpusy1w1ahuap" },
 ];
 
 export const socialServicesData = [
